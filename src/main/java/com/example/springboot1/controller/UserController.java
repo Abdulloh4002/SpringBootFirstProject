8267bdc7cbcb87c57515c746269803cbf1fce50b
abdulloh muradullaev <muradullaevabdulloh4002@gmail.com>
2024-01-12 18:01:32 +0500

    Fixed User model and Controller

--- src/main/java/com/example/springboot1/controller/UserController.java
+++ src/main/java/com/example/springboot1/controller/UserController.java
@@ -1,7 +1,8 @@
 package com.example.springboot1.controller;
 
 
-import org.apache.catalina.User;
+//import
+import com.example.springboot1.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,6 @@ public class UserController {
 
     @GetMapping
     public User getAllUser(){
-        return new User("Imron",15);
+        return new User("imron", 15);
     }
 }
--- src/main/java/com/example/springboot1/model/User.java
+++ src/main/java/com/example/springboot1/model/User.java
@@ -1,5 +1,14 @@
 package com.example.springboot1.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
-    `
+    private String name;
+    private int age;
+
 }
